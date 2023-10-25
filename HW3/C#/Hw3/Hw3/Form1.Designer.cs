9c2db02f05c5e5591c4ab2892498b921eb316691
deto23 <97342146+deto23@users.noreply.github.com>
2023-10-25 19:02:07 +0200

    Add files via upload

---
+++ HW3/C#/Hw3/Hw3/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace Hw3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart4 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart5 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-9, 31);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1355, 670);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(314, 8);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(571, 10);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Number of Systems:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 12);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Number of Attacks:";
+            // 
+            // chart1
+            // 
+            this.chart1.BackImageWrapMode = System.Windows.Forms.DataVisualization.Charting.ChartImageWrapMode.TileFlipXY;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(18, 39);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(527, 354);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(18, 406);
+            this.chart2.Margin = new System.Windows.Forms.Padding(2);
+            this.chart2.Name = "chart2";
+            this.chart2.Size = new System.Drawing.Size(421, 289);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            // 
+            // chart3
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart3.Legends.Add(legend3);
+            this.chart3.Location = new System.Drawing.Point(453, 406);
+            this.chart3.Margin = new System.Windows.Forms.Padding(2);
+            this.chart3.Name = "chart3";
+            this.chart3.Size = new System.Drawing.Size(454, 289);
+            this.chart3.TabIndex = 9;
+            this.chart3.Text = "chart3";
+            // 
+            // chart4
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart4.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart4.Legends.Add(legend4);
+            this.chart4.Location = new System.Drawing.Point(919, 406);
+            this.chart4.Margin = new System.Windows.Forms.Padding(2);
+            this.chart4.Name = "chart4";
+            this.chart4.Size = new System.Drawing.Size(427, 289);
+            this.chart4.TabIndex = 10;
+            this.chart4.Text = "chart4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(417, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Probability of successful attack";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(885, 9);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 19);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart5
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart5.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart5.Legends.Add(legend5);
+            this.chart5.Location = new System.Drawing.Point(571, 39);
+            this.chart5.Name = "chart5";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart5.Series.Add(series1);
+            this.chart5.Size = new System.Drawing.Size(431, 354);
+            this.chart5.TabIndex = 13;
+            this.chart5.Text = "chart5";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(800, 9);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(68, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(738, 13);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Nth Attack";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 690);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.chart5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart4);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart4;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.TextBox textBox3;
+        protected internal System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart5;
+        public System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ HW3/C#/Hw3/Hw3/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Globalization;
+
+namespace Hw3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+
+            textBox1.Text = "10";
+            textBox2.Text = "10";
+            textBox3.Text = "0.6";
+            textBox4.Text = "1";
+
+        }
+
+        public static Random random = new Random();
+
+
+
+        //fillChart method
+        private void fillChart()
+        {
+
+            int numberOfAttacks = int.Parse(textBox2.Text);
+            int numberOfSystems = int.Parse(textBox1.Text);
+            float probability;
+
+            //int indexNthAttack = 3;
+
+            if (float.TryParse(textBox3.Text, NumberStyles.Float, CultureInfo.InvariantCulture, out probability))
+            {
+                Console.WriteLine("Conversione riuscita. Valore float: " + probability);
+            }
+            else
+            {
+                Console.WriteLine("Conversione non riuscita. L'input non è un valore float valido.");
+            }
+
+            int nthAttack_value = int.Parse(textBox4.Text);
+
+            //min value of probability
+            float minValue = 0;
+            //max value of probability
+            float maxValue = 1;
+
+
+
+            int[] x = generateX(numberOfAttacks);
+            int[] y;
+            double[] cumulatedFrequency;
+            double[] relativeFrequency;
+            double[] normalizedRatio;
+
+            int[] lastValues = new int[numberOfSystems];
+            int[] nthAttacks = new int[numberOfSystems];
+
+            
+
+            int min;
+            int max;
+
+            chart1.Series.Clear();
+            chart2.Series.Clear();
+            chart3.Series.Clear();
+            chart4.Series.Clear();
+
+            chart5.Series.Clear();
+
+
+            (int[], double[], double[], double[]) result;
+
+
+
+            for (int i = 0; i < numberOfSystems; i++)
+            {
+                result = generateCoordinateVector(numberOfAttacks, probability, minValue, maxValue);
+
+                y = result.Item1;
+                cumulatedFrequency = result.Item2;
+                relativeFrequency = result.Item3;
+                normalizedRatio = result.Item4;
+
+                lastValues[i] = y[numberOfAttacks - 1];
+
+                nthAttacks[i] = y[nthAttack_value];
+                //Console.WriteLine(lastValues[i]);
+
+                var series = new Series($"Systems {i+1}");
+                series.ChartType = SeriesChartType.Line;
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+
+                series.Points.DataBindXY(x, y);
+                chart1.Series.Add(series);
+
+                var series2 = new Series($"Systems {i + 1}");
+                series2.ChartType = SeriesChartType.Line;
+                chart2.ChartAreas[0].AxisX.Minimum = 0;
+
+                series2.Points.DataBindXY(x, cumulatedFrequency);
+                chart2.Series.Add(series2);
+
+                var series3 = new Series($"Systems {i + 1}");
+                series3.ChartType = SeriesChartType.Line;
+                chart3.ChartAreas[0].AxisX.Minimum = 0;
+
+                series3.Points.DataBindXY(x, relativeFrequency);
+                chart3.Series.Add(series3);
+
+                var series4 = new Series($"Systems {i + 1}");
+                series4.ChartType = SeriesChartType.Line;
+                chart4.ChartAreas[0].AxisX.Minimum = 0;
+
+                series4.Points.DataBindXY(x, normalizedRatio);
+                chart4.Series.Add(series4);
+            }
+
+            int maxLast = lastValues.Max();
+            int minLast = lastValues.Min();
+
+            int maxNth = nthAttacks.Max();
+            int minNth = nthAttacks.Min();
+
+            if (maxLast >= maxNth) max = maxLast;
+            else max = maxNth;
+
+            if (minLast < minNth) min = minLast;
+            else min = minNth;
+
+            int axesLength = max - min + 1;
+
+            int[] yHistogram = new int[axesLength];
+            int[] xLast = new int[axesLength];
+            int[] xNth = new int[axesLength];
+
+            for (int i = 0; i < axesLength; i++)
+            {
+                yHistogram[i] = maxLast - i;
+            }
+
+            for (int i = 0; i < axesLength; i++)
+            {
+                for (int j = 0; j < lastValues.Length; j++)
+                {
+                    if (yHistogram[i] == lastValues[j])
+                    {
+                        xLast[i]++;
+                    }
+                    if (yHistogram[i] == nthAttacks[j])
+                    {
+                        xNth[i]++;
+                    }
+                }
+            }
+
+            chart5.Series.Clear();
+            chart5.Series.Add("Last Attack");
+            chart5.Series.Add("N-th Attack");
+            chart5.Series["Last Attack"].ChartType = SeriesChartType.Bar;
+            chart5.Series["N-th Attack"].ChartType = SeriesChartType.Bar;
+
+            // Bind data to the chart
+            for (int i = 0; i < yHistogram.Length; i++)
+            {
+                chart5.Series["Last Attack"].Points.AddXY(yHistogram[i], xLast[i]);
+                chart5.Series["N-th Attack"].Points.AddXY(yHistogram[i], xNth[i]);
+            }
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            fillChart();
+        }
+
+
+
+        public static int[] generateX(int size)
+        {
+            int[] x = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                x[i] = i;
+            }
+
+            return x;
+        }
+
+        public static float GenerateRandomDouble(float minValue, float maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentException("minValue must be less than or equal to maxValue");
+
+            float randomValue = (float)random.NextDouble(); // Generates a random double between 0 and 1
+            float range = maxValue - minValue;
+            float scaledValue = randomValue * range;
+            float result = scaledValue + minValue;
+
+            return result;
+        }
+
+        public static (int[], double[], double[], double[]) generateCoordinateVector(int size, float probability, float minValue, float maxValue)
+        {
+            int[] y = new int[size];
+            double[] cumulatedFrequency = new double[size];
+            double[] relativeFrequency = new double[size];
+            double[] normalizedRatio = new double[size];
+            y[0] = 0;
+            int sum = 0;
+            double sumFrequency = 0;
+            float value = 0;
+
+
+            for (int i = 1; i < size; i++)
+            {
+                value = GenerateRandomDouble(minValue, maxValue);
+
+                sum += generateY(value, probability);
+                sumFrequency += generateFrequencyY(value, probability);
+                
+                y[i] = sum;
+                cumulatedFrequency[i] = sumFrequency;
+                relativeFrequency[i] = sumFrequency/i+1;
+                normalizedRatio[i] = sumFrequency / Math.Sqrt(i + 1);
+            }
+
+            return (y, cumulatedFrequency, relativeFrequency, normalizedRatio);
+        }
+
+        public static int generateY(float attack, float probability)
+        {
+            if (attack <= probability) return -1;
+            else return +1;
+        }
+
+        public static int generateFrequencyY(float attack, float probability)
+        {
+            if (attack > probability) return +1;
+            else return 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //timer1.Stop(); // Arresta il Timer se è in esecuzione
+            //timer1.Start(); // Riavvia il Timer
+            fillChart();
+
+        }
+
+
+    }
+}
