fed041fd92952232900fe7be8befe759fc3b99c1
deto23 <97342146+deto23@users.noreply.github.com>
2023-11-01 16:55:38 +0100

    Add files via upload

---
+++ HW4/C#/Homework4/Homework4/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Homework4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "K:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Confirm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Intervals";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(261, 9);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(535, 429);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ HW4/C#/Homework4/Homework4/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Collections;
+
+namespace Homework4
+{
+    public partial class Form1 : Form
+    {
+        private List<Label> labels = new List<Label>();
+        private List<TextBox> intervals = new List<TextBox>();
+        private List<double[]> variables = new List<double[]>();
+        private Button generateButton;
+
+        private int numberOfValues = 100;
+
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "2";
+
+            //WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            int n = int.Parse(textBox1.Text);
+
+            RemoveExcessControls();
+
+            GenerateVariables(n);
+        }
+
+        private void GenerateVariables(int n)
+        {
+            if (generateButton != null)
+            {
+                Controls.Remove(generateButton);
+                generateButton.Dispose();
+                generateButton = null;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Variable " + (i + 1) + ":");
+
+                double[] values = new double[numberOfValues];
+
+                Label label = new Label();
+                label.Text = "Variable " + (i + 1) + ":";
+                label.Location = new Point(20, 50 + i * 30);
+                labels.Add(label);
+
+                TextBox textBox = new TextBox();
+                textBox.Name = "textBoxInterval" + (i + 1);
+                textBox.Location = new Point(120, 50 + i * 30);
+                intervals.Add(textBox);
+
+                this.Controls.Add(label);
+                this.Controls.Add(textBox);
+
+                for (int j = 0; j < numberOfValues; j++)
+                {
+                    double randomValue = random.NextDouble();
+                    Console.WriteLine("Value " + (j + 1) + ": " + randomValue);
+                    values[j] = randomValue;
+                }
+                variables.Add(values);
+            }
+
+            generateButton = new Button();
+            generateButton.Text = "Generate";
+            generateButton.Location = new Point(120, 50 + n * 30);
+            generateButton.Click += CumulativeFrequency_Click;
+            this.Controls.Add(generateButton);
+        }
+
+        private void CumulativeFrequency_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, Dictionary<double, int>> cumulativeFrequency = new Dictionary<int, Dictionary<double, int>>();
+
+            int[] intervalCounts = new int[variables.Count];
+            int i = 0;
+
+            foreach (var interval in intervals)
+            {
+                int intervalValue = 0;
+                string text = interval.Text;
+                if (!string.IsNullOrEmpty(text) && int.TryParse(text, out intervalValue))
+                {
+                    intervalCounts[i] = intervalValue;
+                }
+                else
+                {
+                    intervalCounts[i] = 0;
+                }
+                i++;
+            }
+
+            i = 0;
+
+            foreach (var variable in variables)
+            {
+                Console.WriteLine("Variable "+(i)+" Count values: "+(variable.Length));
+                List<double> intervalsValues = new List<double>();
+                double max = variable.Max();
+                double min = variable.Min();
+                Console.WriteLine("Max: " + max + " - Min: " +min);
+                int numIntervals = intervalCounts[i];
+                double intervalWidth = (max - min) / numIntervals;
+
+                Dictionary<double, int> intervalHistogram = new Dictionary<double, int>();
+
+                for (int j = 0; j < numIntervals; j++)
+                {
+                    double intervalStart = min + j * intervalWidth;
+                    double intervalEnd = intervalStart + intervalWidth;
+
+                    if (j == numIntervals - 1)
+                    {
+                        int count = variable.Count(value => value >= intervalStart && value <= max);
+                        intervalHistogram[intervalStart] = count;
+                    }
+                    else
+                    {
+                        int count = variable.Count(value => value >= intervalStart && value < intervalEnd);
+                        intervalHistogram[intervalStart] = count;
+                    }
+                }
+
+                foreach (var kvp in intervalHistogram)
+                {
+                    Console.WriteLine("Interval " + kvp.Key + ": Count " + kvp.Value);
+                }
+                cumulativeFrequency.Add(i, intervalHistogram);
+                i++;
+            }
+            GenerateKJointDistribution(cumulativeFrequency);
+        }
+
+        private void GenerateKJointDistribution(Dictionary<int, Dictionary<double, int>> cumulativeFrequency)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            List<Dictionary<int, double>> jointDistribution = new List<Dictionary<int, double>>();
+            List<List<double>> variableIntervals = new List<List<double>>();
+
+            int totalCombination = 0;
+            double totalFrequency = 1.0;
+            double percentageFrequency = 0;
+
+            foreach (var variableProbabilities in cumulativeFrequency)
+            {
+                variableIntervals.Add(variableProbabilities.Value.Keys.ToList());
+            }
+
+            for (int i = 0; i < variableIntervals.Count; i++)
+            {
+                dataGridView1.Columns.Add("Variable" + (i + 1), "Variable " + (i + 1));
+            }
+
+            int[] indices = new int[variableIntervals.Count];
+
+            dataGridView1.Columns.Add("Frequency", "Frequency");
+
+            while (true)
+            {
+                totalCombination++;
+                Dictionary<int, double> jointProbabilities = new Dictionary<int, double>();
+                double frequency = 1;
+
+                for (int i = 0; i < variableIntervals.Count; i++)
+                {
+                    jointProbabilities[i] = variableIntervals[i][indices[i]];
+
+                    int variableIndex = cumulativeFrequency.Keys.ElementAt(i);
+                    double intervalValue = jointProbabilities[i];
+
+                    frequency *= cumulativeFrequency[variableIndex][intervalValue];
+                }
+
+                totalFrequency += frequency;
+
+                percentageFrequency = (frequency / totalFrequency) * 100;
+
+                int rowIndex = dataGridView1.Rows.Add();
+
+                for (int i = 0; i < variableIntervals.Count; i++)
+                {
+                    dataGridView1.Rows[rowIndex].Cells[i].Value = jointProbabilities[i];
+                }
+
+                dataGridView1.Rows[rowIndex].Cells["Frequency"].Value = percentageFrequency.ToString("F2") + " %";
+
+                int incrementIndex = variableIntervals.Count - 1;
+                indices[incrementIndex]++;
+
+                while (incrementIndex >= 0 && indices[incrementIndex] >= variableIntervals[incrementIndex].Count)
+                {
+                    indices[incrementIndex] = 0;
+                    incrementIndex--;
+
+                    if (incrementIndex >= 0)
+                    {
+                        indices[incrementIndex]++;
+                    }
+                }
+
+                if (incrementIndex < 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Total combination: " + totalCombination);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+
+            RemoveExcessControls();
+
+            GenerateVariables(n);
+        }
+
+        private void RemoveExcessControls()
+        {
+            for (int i = 0; i < labels.Count; i++)
+            {
+                Controls.Remove(labels[i]);
+                labels[i].Dispose();
+
+                Controls.Remove(intervals[i]);
+                intervals[i].Dispose();
+            }
+
+            labels.Clear();
+            intervals.Clear();
+            variables.Clear();
+        }
+    }
+}
