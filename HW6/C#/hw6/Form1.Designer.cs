55347c33807d37c6161885bc2b8ea33d1ca6ec10
deto23 <97342146+deto23@users.noreply.github.com>
2023-11-15 21:43:57 +0100

    Add files via upload

---
+++ HW6/C#/hw6/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace hw6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1143, 525);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(64, 75);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 12;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(429, 75);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(300, 300);
+            this.chart2.TabIndex = 13;
+            this.chart2.Text = "chart2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(175, 6);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "M:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(151, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "N:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(294, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "p:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(316, 6);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 19;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(436, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "S:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(459, 6);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(585, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Simulate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Percentage of unsecure system:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 590);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ HW6/C#/hw6/Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace hw6
+{
+    public partial class Form1 : Form
+    {
+
+        private Chart[] charts;
+        private Point[] chartLocations;
+        private Point[] mouseDownLocations;
+        private bool[] isDraggingList;
+        private bool[] isResizingList;
+        private Size[] resizeStartSize;
+
+        private static int discarded;
+        private static Dictionary<int, int> histogramData = Enumerable.Range(0, 9)
+            .ToDictionary(i => -(i + 2) * 10, i => 0);
+
+
+        public static Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Dock = DockStyle.Fill;
+
+            textBox1.Text = "10";
+            textBox2.Text = "250";
+            textBox3.Text = "0,5";
+            textBox4.Text = "20";
+
+            int numberOfCharts = 2;
+            charts = new Chart[numberOfCharts];
+            chartLocations = new Point[numberOfCharts];
+            mouseDownLocations = new Point[numberOfCharts];
+            isDraggingList = new bool[numberOfCharts];
+            isResizingList = new bool[numberOfCharts];
+            resizeStartSize = new Size[numberOfCharts];
+
+            for (int i = 0; i < numberOfCharts; i++)
+            {
+                charts[i] = Controls.Find($"chart{i + 1}", true)[0] as Chart;
+                chartLocations[i] = new Point(0, 0);
+                mouseDownLocations[i] = Point.Empty;
+                isDraggingList[i] = false;
+
+                charts[i].MouseDown += Chart_MouseDown;
+                charts[i].MouseMove += Chart_MouseMove;
+                charts[i].MouseUp += Chart_MouseUp;
+            }
+        }
+
+        //------------------RESIZE E MOVE CHART------------------------
+        private void Chart_MouseDown(object sender, MouseEventArgs e)
+        {
+            var chart = (Chart)sender;
+            int chartIndex = Array.IndexOf(charts, chart);
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (e.X >= chart.Width - 10 && e.Y >= chart.Height - 10)
+                {
+                    isResizingList[chartIndex] = true;
+                    resizeStartSize[chartIndex] = chart.Size;
+                }
+                else
+                {
+                    isDraggingList[chartIndex] = true;
+                    mouseDownLocations[chartIndex] = e.Location;
+                }
+            }
+        }
+
+        private void Chart_MouseMove(object sender, MouseEventArgs e)
+        {
+            var chart = (Chart)sender;
+            int chartIndex = Array.IndexOf(charts, chart);
+
+            if (isDraggingList[chartIndex])
+            {
+                int deltaX = e.X - mouseDownLocations[chartIndex].X;
+                int deltaY = e.Y - mouseDownLocations[chartIndex].Y;
+
+                chartLocations[chartIndex].X += deltaX;
+                chartLocations[chartIndex].Y += deltaY;
+
+                if (chartLocations[chartIndex].X < 0) chartLocations[chartIndex].X = 0;
+                if (chartLocations[chartIndex].Y < 0) chartLocations[chartIndex].Y = 0;
+                if (chartLocations[chartIndex].X + chart.Width > pictureBox1.Width) chartLocations[chartIndex].X = pictureBox1.Width - chart.Width;
+                if (chartLocations[chartIndex].Y + chart.Height > pictureBox1.Height) chartLocations[chartIndex].Y = pictureBox1.Height - chart.Height;
+
+                chart.Location = chartLocations[chartIndex];
+            }
+            else if (isResizingList[chartIndex])
+            {
+                int deltaX = e.X - resizeStartSize[chartIndex].Width;
+                int deltaY = e.Y - resizeStartSize[chartIndex].Height;
+
+                int newWidth = resizeStartSize[chartIndex].Width + deltaX;
+                int newHeight = resizeStartSize[chartIndex].Height + deltaY;
+
+                if (newWidth < 100)
+                    newWidth = 100;
+                if (newHeight < 100)
+                    newHeight = 100;
+
+                chart.Size = new Size(newWidth, newHeight);
+            }
+            else if (e.X >= chart.Width - 10 && e.Y >= chart.Height - 10)
+            {
+                chart.BackColor = Color.LightGray;
+                chart.Cursor = Cursors.SizeNWSE;
+            }
+            else
+            {
+                chart.BackColor = Color.White;
+                chart.Cursor = Cursors.Default;
+            }
+        }
+
+        private void Chart_MouseUp(object sender, MouseEventArgs e)
+        {
+            var chart = (Chart)sender;
+            int chartIndex = Array.IndexOf(charts, chart);
+
+            if (e.Button == MouseButtons.Left)
+            {
+                isDraggingList[chartIndex] = false;
+                isResizingList[chartIndex] = false;
+            }
+        }
+        //------------------------------------------------------
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fillChart();
+        }
+
+        private void fillChart()
+        {
+            //min value of probability
+            float minValue = 0;
+            //max value of probability
+            float maxValue = 1;
+
+            int numberOfSystems = int.Parse(textBox1.Text); //M
+            int numberOfAttacks = int.Parse(textBox2.Text);   //N
+            float probability = float.Parse(textBox3.Text);  //p
+            int securityScore = int.Parse(textBox4.Text);   //S
+
+            discarded = 0;
+            histogramData = Enumerable.Range(0, 9)
+            .ToDictionary(i => -(i + 2) * 10, i => 0);
+
+        int[] x = generateX(numberOfAttacks);
+            int[] y;
+
+            chart1.Series.Clear();
+
+            for (int i = 0; i < numberOfSystems; i++)
+            {
+                List<int> penetrationScores = new List<int> { -20, -30, -40, -50, -60, -70, -80, -90, -100};
+
+                y = generateCoordinateVector(numberOfAttacks, probability, minValue, maxValue, securityScore, penetrationScores);
+
+                var series = new Series($"Server {i + 1}");
+                series.ChartType = SeriesChartType.Line;
+                chart1.ChartAreas[0].AxisX.Minimum = 0;
+
+                series.Points.DataBindXY(x, y);
+                chart1.Series.Add(series);
+            }
+            
+
+            float percentage = (((float)discarded / (float)numberOfSystems) * 100);
+            Console.WriteLine(percentage);
+            label1.Text = "Percentage of unsecure system: " + percentage.ToString() + "%";
+
+            CalculateSystemDiscard(numberOfSystems);
+
+        }
+
+        private void CalculateSystemDiscard(int numberOfSystems)
+        {
+            CreateHistogramChart(numberOfSystems);
+        }
+
+        private void CreateHistogramChart(int numberOfSystems)
+        {
+            chart2.Series.Clear();
+
+            // Creazione della serie
+            var series = new Series("Unsecure Score")
+            {
+                ChartType = SeriesChartType.Column
+            };
+
+            // Aggiunta dei punti dati alla serie
+            foreach (var dataPoint in histogramData)
+            {
+                series.Points.AddXY(dataPoint.Key.ToString(), dataPoint.Value);
+            }
+
+            // Aggiunta della serie al grafico
+            chart2.Series.Add(series);
+
+            chart2.ChartAreas[0].AxisY.Minimum = 0; // Valore minimo dell'asse y
+            chart2.ChartAreas[0].AxisY.Maximum = numberOfSystems; // Valore massimo dell'asse y
+            chart2.ChartAreas[0].AxisY.Interval = 1; // Intervallo tra i tick dell'asse y
+
+            // Impostazione delle opzioni del grafico
+            chart2.ChartAreas[0].AxisX.Minimum = 0;
+
+            // Visualizzazione del grafico
+            chart2.Show();
+        }
+
+
+        public static float GenerateRandomDouble(float minValue, float maxValue)
+        {
+            if (minValue > maxValue)
+                throw new ArgumentException("minValue must be less than or equal to maxValue");
+
+            float randomValue = (float)random.NextDouble(); // Generates a random double between 0 and 1
+            float range = maxValue - minValue;
+            float scaledValue = randomValue * range;
+            float result = scaledValue + minValue;
+
+            return result;
+        }
+
+        public static int[] generateCoordinateVector(int size, float probability, float minValue, float maxValue, int securityScore, List<int> penetrationScores)
+        {
+            int[] y = new int[size];
+            y[0] = 0;
+            int sum = 0;
+            float value;
+            bool systemChecked = false;
+            bool systemSafe = false;
+
+
+            for (int i = 1; i < size; i++)
+            {
+                value = GenerateRandomDouble(minValue, maxValue);
+
+                sum += generateY(value, probability);
+
+                if (sum == securityScore)
+                {
+                    systemChecked = true;
+                    systemSafe = true;
+                }
+                else if (sum == penetrationScores[0])
+                {
+                    if (!systemChecked)
+                    {
+                        systemChecked = true;
+                        discarded += 1;
+                    }
+                    else if (systemSafe == true)
+                    {
+                        break;
+                    }
+                    histogramData[penetrationScores[0]] += 1;
+                    penetrationScores.RemoveAt(0);
+                }
+
+                y[i] = sum;
+            }
+
+            return y;
+        }
+
+        public static Dictionary<int, int> createDictionary(int[] penetrationScores)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            foreach (int score in penetrationScores)
+            {
+                dict[score] = 0;
+            }
+
+            return dict;
+        }
+
+        public static Dictionary<int, int> CheckDict(Dictionary<int, int> dict, int value)
+        {
+            foreach (var key in dict.Keys.ToList())
+            {
+                if (value <= key)
+                {
+                    dict[key] = 1;
+                }
+            }
+
+            return dict;
+        }
+
+        public static int[] generateX(int size)
+        {
+            int[] x = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                x[i] = i;
+            }
+
+            return x;
+        }
+
+        public static int generateY(float attack, float probability)
+        {
+            if (attack <= probability) return -1;
+            else return 1;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            fillChart();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
